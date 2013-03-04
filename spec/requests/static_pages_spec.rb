669b14458b2debf5770a0ca1245dc0d96052f77f
paul-riley <paul@hookedoncaffeine.com>
2013-03-04 11:34:06 -0600

    Finish Lab 6.2 Testing Links

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -16,6 +16,18 @@ describe "Static pages" do
 
     it_should_behave_like "all static pages"
     it { should_not have_selector 'title', text: '| Home' }
+    it "should have the right links on the layout" do
+	visit root_path
+	click_link "About"
+	page.should have_selector 'title', text: full_title('About Us')
+	click_link "Contact"
+        page.should have_selector 'title', text: full_title('Contact')
+	click_link "Home"
+	click_link "Sign up now!"
+	page.should have_selector 'title', text: full_title('Sign up')
+	click_link "sample app"
+        page.should have_selector 'title', text: full_title('')
+  	end
   end
 
   describe "Help page" do
