ce1f3b5d86ace98990924ce06d25f7ab52097b9a
paul-riley <paul@hookedoncaffeine.com>
2013-04-13 19:22:48 -0500

    Fix Test for Ch 8.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+  before_filter :signed_in_user, only: [:edit, :update]
+  
   def show
 	@user = User.find(params[:id])
   end
@@ -19,4 +20,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  private
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in? 
+    end
 end
