1ec648f108fa7bd6d90c09ba1980f800f42b9e73
paul-riley <paul@hookedoncaffeine.com>
2013-05-11 17:05:51 -0500

    Finished Lab 10.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,11 @@ class UsersController < ApplicationController
   end
 
   def new
-    @user = User.new
+    if signed_in?
+      redirect_to root_path, notice: "You are already signed in"
+    else
+      @user = User.new
+    end
   end
 
   def index
@@ -16,13 +20,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+    if signed_in?
+      redirect_to root_path, notice: "You are already signed in" 
     else
-      render 'new'
+       @user = User.new(params[:user])
+       if @user.save
+          sign_in @user
+          flash[:success] ="Welcome to the Sample App!"
+          redirect_to @user
+       else
+         render 'new'
+       end
     end
   end
 
